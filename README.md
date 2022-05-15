536334a967b37fe0150a939f7f98a5317d02b7c9
jamieboy1337 <jamieboy1337@gmail.com>
2022-05-14 21:04:34 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# chitchat
+
+a simple C# TTS application which leverages OS-specific APIs to generate and play back text as speech, including a scripting language for generating your own text
---
+++ chitchat/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// todo: i think i can run this on win aat some point and include those necessary tts libs, for now its just mac :(
+
+namespace chitchat
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
